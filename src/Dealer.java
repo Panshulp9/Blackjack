a5b18275a2f61f4683f317450203b7d987599bfc
Panshulp <panshul_purusharthy27@milton.edu>
2026-05-14 20:53:23 -0400

    filled instance

--- src/Dealer.java
+++ src/Dealer.java
@@ -5,9 +5,17 @@ public class Dealer {
     public boolean isBust;
 
     public Dealer(){
-
+        hand = new Card[2];
         cardTotal = 0;
         isBust = false;
         isOver16 = false;
+    }
+    public void printInfo(){
+        System.out.println("The card total:" + cardTotal);
+        System.out.println("is busted: " + isBust);
+        System.out.println("is over 16: " + isOver16);
+        for(int z = 0; z < hand.length; z++){
+            System.out.println(hand[z]);
+        }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -6,12 +6,19 @@ public class Player {
     public String name;
 
     public Player(){
-
+        hand = new Card[2];
         cardTotal = 0;
         isBust = false;
         isHit = false;
         name = "Player 1";
     }
-
-
+    public void printInfo(){
+        System.out.println("The card total:" + cardTotal);
+        System.out.println("is busted: " + isBust);
+        System.out.println("is hit: " + isHit);
+        System.out.println(name);
+        for(int z = 0; z < hand.length; z++){
+            System.out.println(hand[z]);
+        }
+    }
 }
